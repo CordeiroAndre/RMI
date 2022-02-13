55420eb2b4d40ee161fc1421e362a7c493096661
Andre <60711778+CordeiroAndre@users.noreply.github.com>
2022-02-13 16:54:31 -0300

    Finalização da UI de cliente 2 kkk

--- Implementation/src/client/Client.java
+++ Implementation/src/client/Client.java
@@ -8,14 +8,7 @@ import java.rmi.RemoteException;
 
 
 public class Client {
-
-
-
-
-
-
     public static void main(String[] args) throws RemoteException {
        ClientWindow clientWindow = new ClientWindow();
-
     }
 }
